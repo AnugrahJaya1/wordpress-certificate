0dc34a84b3191fe637c5763fd10d1b309e80714b
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-02 18:04:50 +0700

    add custom font from external

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -10,6 +10,9 @@ function university_files()
     // icon
     wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"); // nickname, location
 
+    // font
+    wp_enqueue_style("custom-google-fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i"); // nickname, location
+
     // js
 }
 
