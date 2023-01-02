1bd11c94a679555f0cdd37488d82bb3c83eae309
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-02 18:02:36 +0700

    add icon from external

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -7,6 +7,9 @@ function university_files()
     wp_enqueue_style("university_main_style", get_theme_file_uri("/build/style-index.css")); // nickname, location
     wp_enqueue_style("university_extra_style", get_theme_file_uri("/build/index.css")); // nickname, location
 
+    // icon
+    wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"); // nickname, location
+
     // js
 }
 
