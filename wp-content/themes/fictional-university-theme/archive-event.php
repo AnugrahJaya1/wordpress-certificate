26f0ec16e06c0958bf3595d11fc44cef62993627
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-07 13:13:21 +0700

    change banner

--- wp-content/themes/fictional-university-theme/archive-event.php
+++ wp-content/themes/fictional-university-theme/archive-event.php
@@ -5,18 +5,11 @@ get_header();
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-            <?php
-            // show archive title
-            the_archive_title();
-            ?>
+            All Events
         </h1>
         <div class="page-banner__intro">
             <p>
-                <?php
-                // show archive description
-                // need to add description in admin page
-                the_archive_description("");
-                ?>
+                See what is going on in our world.
             </p>
         </div>
     </div>
