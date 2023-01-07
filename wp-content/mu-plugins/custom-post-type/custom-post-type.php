8d3ad8940d552a99ffd8c2852abb7eeba5055abf
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-07 12:17:27 +0700

    move add event post from theme to mu-plugins, so events are not dependent of the theme

---
+++ wp-content/mu-plugins/custom-post-type/custom-post-type.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @package Custom Post Types
+ */
+
+/*
+Plugin Name: Custom Post Types
+Plugin URI: 
+Description: Custom Post Types
+Author: Jaya
+Version: 1.0.0
+Text Domain: Jaya
+*/
+
+/**
+ * If ABSPATH not defined
+ * Destroy the plugin
+ * or Exit if accessed directly.
+ */
+if (!defined('ABSPATH')) {
+    die;
+}
+
+
+class Custom_Post_Type
+{
+
+    function __construct()
+    {
+        add_action("init", [$this, "event_post_type"]);
+    }
+
+    /**
+     * Add menu page
+     * Flush rewrite rules
+     */
+    function activate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Flush rewrite rules
+     */
+    function deactivate()
+    {
+        flush_rewrite_rules();
+    }
+
+    function event_post_type()
+    {
+        // add custom post type
+        {
+            // add event post type
+            register_post_type("event", [
+                "public" => true, // show in admin nav bar
+                "labels" => [
+                    "name" => "Events", // name showed in admin nav bar
+                ],
+                "menu_icon" => "dashicons-calendar"
+            ]); // name of post type, argument
+        }
+    }
+}
+
+if (class_exists("Custom_Post_Type")) {
+    // initialize class
+    $custom_post_type = new Custom_Post_Type();
+}
+
+/**
+ * Activation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_activation_hook(__FILE__, array($custom_post_type, 'activate'));
+
+/**
+ * Deactivation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_deactivation_hook(__FILE__, array($custom_post_type, 'deactivate'));
