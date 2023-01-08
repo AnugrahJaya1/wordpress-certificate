0f505fcdff37cfe271dad0c1582c6c968492a154
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-08 16:34:12 +0700

    add feature to resize image when upload

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -29,6 +29,10 @@ function university_features()
     // add post thumbnails
     add_theme_support("post-thumbnails"); // by default add to post post_type/blog
 
+    // resize size
+    add_image_size("professor-landscape", 400, 260, true); // nickname, w, h, crop? (false)
+    add_image_size("professor-portrait", 480, 650, true); // nickname, w, h, crop? (false)
+
     // add header menu
     register_nav_menu("header-menu-location", "Header Menu Location"); // location/slug, name
 
