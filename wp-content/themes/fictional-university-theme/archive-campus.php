4ef75dffcef657a35b9946ee5ca26ec968fa31c5
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-10 14:07:50 +0700

    create tempalte body of archive campus

--- wp-content/themes/fictional-university-theme/archive-campus.php
+++ wp-content/themes/fictional-university-theme/archive-campus.php
@@ -7,26 +7,25 @@ page_banner([
 ?>
 <!-- content -->
 <div class="container container--narrow page-section">
-    <ul class="link-list min-list">
+    <div class="acf-map">
         <?php
         while (have_posts()) {
             // get post
             the_post(); // keep track what post we use
+
+            $map_location = get_field("map_location");
+            $lat = empty($map_location["lat"]) ? "6.9039" : $map_location["lat"];
+            $lng = empty($map_location["lng"]) ? "107.6491" : $map_location["lng"];
         ?>
-            <li>
-                <a href="<?php the_permalink(); ?>">
-                    <?php
-                    the_title();
-                    ?>
-                </a>
-            </li>
+            <div class="marker" data-lat="<?php echo $lat ?>" data-lng="<?php echo $lng ?>">
+
+            </div>
         <?php
         }
         //  add pagination
         echo paginate_links();
         ?>
-    </ul>
-    </p>
+    </div>
 </div>
 
 <?php
