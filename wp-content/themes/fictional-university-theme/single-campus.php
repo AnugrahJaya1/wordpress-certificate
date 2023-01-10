6cd17ffea9dc5ad499f9efa4bade0d719a6decad
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-10 16:07:21 +0700

    remove related event in single campus

--- wp-content/themes/fictional-university-theme/single-campus.php
+++ wp-content/themes/fictional-university-theme/single-campus.php
@@ -84,48 +84,6 @@ while (have_posts()) {
         }
         // reset our custom queries
         wp_reset_postdata();
-
-
-        $today = date("Ymd");
-
-        // need update re-save permalink
-        // by default order by publish date/post date
-        // get events post
-        $home_page_events = new WP_Query([
-            "posts_per_page" => 2, // show only 2 post
-            "post_type" => "event",
-            "orderby" => "meta_value_num", // default -> post date, reverse alphabet. meta_value -> get meta value,
-            "meta_key" => "event_date",
-            "meta_query" => [
-                // only get upcoming event not the past
-                [
-                    // greater than today
-                    "key" => "event_date", // custom field
-                    "compare" => ">=",
-                    "value" => $today, // YYYYmmdd
-                    "type" => "numeric"
-                ],
-                [
-                    "key" => "related_program", // custom field
-                    "compare" => "LIKE",
-                    "value" => '"' . get_the_ID() . '"', // search "ID"
-                ]
-            ],
-            "order" => "ASC", // default DESC
-        ]);
-
-        if ($home_page_events->have_posts()) {
-            echo "<hr class='section-break'>";
-            echo "<h2 class='headline headline--medium'>Upcoming " . get_the_title() . " Events</h2>";
-            while ($home_page_events->have_posts()) {
-                $home_page_events->the_post();
-
-                // load .php file with html content
-                get_template_part("template-parts/content", get_post_type()); // slug/location, -file name
-            }
-        }
-        // reset our custom queries
-        wp_reset_postdata();
         ?>
     </div>
 <?php
