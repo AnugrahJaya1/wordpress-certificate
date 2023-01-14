01aea08ca9c3e0b319d760d8109450dbe907d6fe
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-14 15:15:36 +0700

    load css for login/register page

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -206,4 +206,19 @@ function our_header_url()
     return esc_url(site_url("/"));
 }
 
-add_filter("login_headerurl", "our_header_url");//object want change, callback function
+add_filter("login_headerurl", "our_header_url"); //object want change, callback function
+
+function our_login_css()
+{
+    // css
+    wp_enqueue_style("university_main_style", get_theme_file_uri("/build/style-index.css")); // nickname, location
+    wp_enqueue_style("university_extra_style", get_theme_file_uri("/build/index.css")); // nickname, location
+
+    // icon
+    wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"); // nickname, location
+
+    // font
+    wp_enqueue_style("custom-google-fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i"); // nickname, location
+}
+
+add_action("login_enqueue_scripts", "our_login_css");
