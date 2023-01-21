94253f28504a6e2d4e0d04364ec37ef63a47bffd
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-21 11:03:41 +0700

    make class for plugin

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -7,22 +7,66 @@
  * Author: Jaya
  */
 
-add_action("admin_menu", "our_plugin_settings_link");
-
-function our_plugin_settings_link()
+class WordCountAndTimePlugin
 {
-    add_options_page(
-        "Word Count Settings", //title (head)
-        "Word Count", // title (setting section)
-        "manage_options", //capability
-        "word-count-settings-page", //slug
-        "our_settings_page_HTML" //call back
-    );
-}
+    function __construct()
+    {
+        add_action("admin_menu", array($this, "admin_page"));
+    }
 
-function our_settings_page_HTML()
-{
+    function admin_page()
+    {
+        add_options_page(
+            "Word Count Settings", //title (head)
+            "Word Count", // title (setting section)
+            "manage_options", //capability
+            "word-count-settings-page", //slug
+            array($this, "our_HTML") //call back
+        );
+    }
+
+    function our_HTML()
+    {
 ?>
-TEST
+        <div class="wrap">
+            <h1>Word Count Settings</h1>
+        </div>
 <?php
+    }
+
+    /**
+     * Add menu page
+     * Flush rewrite rules
+     */
+    function activate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Flush rewrite rules
+     */
+    function deactivate()
+    {
+        flush_rewrite_rules();
+    }
 }
+
+if (class_exists("WordCountAndTimePlugin")) {
+    // initialize class
+    $word_count = new WordCountAndTimePlugin();
+}
+
+/**
+ * Activation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_activation_hook(__FILE__, array($word_count, 'activate'));
+
+/**
+ * Deactivation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_deactivation_hook(__FILE__, array($word_count, 'deactivate'));
