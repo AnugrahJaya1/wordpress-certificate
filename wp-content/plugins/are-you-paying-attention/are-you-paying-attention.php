a884d3d8d38fd249cfc620e78790e95f32885e28
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-22 13:26:26 +0700

    add simple plugin template to add wp block for post

---
+++ wp-content/plugins/are-you-paying-attention/are-you-paying-attention.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Plugin Name: Are You Paying Attention Quiz
+ * Description: Are You Paying Attention Quiz
+ * Version: 1.0
+ * Author: Jaya
+ */
+
+if (!defined("ABSPATH")) {
+    exit;
+}
+
+class AreYouPayingAttentionQuiz
+{
+    function __construct()
+    {
+        add_action("enqueue_block_editor_assets", array($this, "admin_assets"));
+    }
+
+    function admin_assets(){
+        // load js
+        wp_enqueue_script("our_new_block_type", plugin_dir_url(__FILE__)."/wp-blocks/test.js", array("wp-blocks"));
+    }
+
+    /**
+     * Add menu page
+     * Flush rewrite rules
+     */
+    function activate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Flush rewrite rules
+     */
+    function deactivate()
+    {
+        flush_rewrite_rules();
+    }
+}
+
+
+if (class_exists("AreYouPayingAttentionQuiz")) {
+    // initialize class
+    $quiz = new AreYouPayingAttentionQuiz();
+}
+
+/**
+ * Activation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_activation_hook(__FILE__, array($quiz, 'activate'));
+
+/**
+ * Deactivation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_deactivation_hook(__FILE__, array($quiz, 'deactivate'));
