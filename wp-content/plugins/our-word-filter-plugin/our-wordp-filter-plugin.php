60c30e1d2aa359d0c06083301ee68832630b7e3e
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-22 09:30:10 +0700

    create simple class template

--- wp-content/plugins/our-word-filter-plugin/our-wordp-filter-plugin.php
+++ wp-content/plugins/our-word-filter-plugin/our-wordp-filter-plugin.php
@@ -10,3 +10,47 @@
 if (!defined("ABSPATH")) {
     exit;
 }
+
+class OurWordFilterPlugin
+{
+    function __construct()
+    {
+    }
+
+    /**
+     * Add menu page
+     * Flush rewrite rules
+     */
+    function activate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Flush rewrite rules
+     */
+    function deactivate()
+    {
+        flush_rewrite_rules();
+    }
+}
+
+
+if (class_exists("OurWordFilterPlugin")) {
+    // initialize class
+    $word_filter = new OurWordFilterPlugin();
+}
+
+/**
+ * Activation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_activation_hook(__FILE__, array($word_filter, 'activate'));
+
+/**
+ * Deactivation
+ * @param __FILE__ : this file
+ * @param array (class, function)
+ */
+register_deactivation_hook(__FILE__, array($word_filter, 'deactivate'));
