349081880c3b19f3a2a555b1d8c729ccac078bd1
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-23 13:44:29 +0700

    show correct and wrong answer after user choose correct answer

--- wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'f4421775a5e23586359d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1dc15ca165db98c2164c');
--- wp-content/plugins/are-you-paying-attention/build/frontend.js
+++ wp-content/plugins/are-you-paying-attention/build/frontend.js
@@ -138,11 +138,17 @@ div_to_update.forEach(function (div) {
 });
 function Quiz(props) {
   const [is_correct, set_is_correct] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(undefined);
+  const [is_correct_delayed, set_is_correct_delayed] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(undefined);
   (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(() => {
     if (is_correct == false) {
       setTimeout(() => {
         set_is_correct(undefined);
       }, 2600);
+    }
+    if (is_correct == true) {
+      setTimeout(() => {
+        set_is_correct_delayed(true);
+      }, 1000);
     }
   },
   // function
@@ -160,8 +166,25 @@ function Quiz(props) {
     className: "paying-attention-frontend"
   }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, props.question), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("ul", null, props.answers.map(function (answer, index) {
     return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("li", {
+      className: (is_correct_delayed == true && index == props.correct_answer ? "no-click " : "") + (is_correct_delayed == true && index != props.correct_answer ? "fade-incorrect" : ""),
       onClick: is_correct == true ? undefined : () => handle_answer(index)
-    }, answer);
+    }, is_correct_delayed == true && index == props.correct_answer && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "20",
+      height: "20",
+      className: "bi bi-check",
+      viewBox: "0 0 16 16"
+    }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+      d: "M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"
+    })), is_correct_delayed == true && index != props.correct_answer && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "20",
+      height: "20",
+      className: "bi bi-x",
+      viewBox: "0 0 16 16"
+    }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+      d: "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+    })), answer);
   })), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
     className: "correct-message " + (is_correct == true ? "correct-message--visible" : "")
   }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
