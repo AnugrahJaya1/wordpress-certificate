05dcc4c46bc19465be1a047db9ad5a026156df34
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-24 10:13:42 +0700

    implement loading list  of professor

--- wp-content/plugins/featured-professor/build/index.asset.php
+++ wp-content/plugins/featured-professor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'f62e0a01fcfb9e6821ae');
+<?php return array('dependencies' => array('wp-data', 'wp-element'), 'version' => 'dae3c3045a7ccc7680da');
