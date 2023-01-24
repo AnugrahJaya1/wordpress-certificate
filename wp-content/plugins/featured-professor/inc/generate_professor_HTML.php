5422b16fadafb7eb7cac6a7f8b2f0d87c06baeb5
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-24 10:41:12 +0700

    implement displaying professor info in FE

--- wp-content/plugins/featured-professor/inc/generate_professor_HTML.php
+++ wp-content/plugins/featured-professor/inc/generate_professor_HTML.php
@@ -10,8 +10,42 @@ function generate_professor_HTML($id)
     while ($prof->have_posts()) {
         $prof->the_post();
         ob_start(); ?>
+        <div class="professor-callout">
+            <div class="professor-callout__photo" style="background-image: url(<?php the_post_thumbnail_url("professor-portrait") ?>);">
 
-        <?php
+            </div>
+            <div class="professor-callout__text">
+                <h5>
+                    <?php the_title(); ?>
+                </h5>
+                <p>
+                    <?php
+                    echo wp_trim_words(get_the_content(), 30);
+                    ?>
+                </p>
+
+                <?php
+                $related_programs = get_field("related_program");
+                if ($related_programs) {
+                    echo "<p>Name teachers: ";
+                    foreach ($related_programs as $key => $program) {
+                        echo get_the_title($program);
+                        if ($key != array_key_last($related_programs) && count($related_programs) > 1) {
+                            echo ", ";
+                        }
+                    }
+                    echo ".</p>";
+                }
+                ?>
+
+                <p>
+                    <strong>
+                        <a href="<?php the_permalink(); ?>">Learn more about <?php the_title(); ?> &raquo; </a>
+                    </strong>
+                </p>
+            </div>
+        </div>
+<?php
         wp_reset_postdata();
         return ob_get_clean();
     }
