6010c2ae5e45a1197a69d80863aeef6dc3cdaf09
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-27 11:33:22 +0700

    add template banner jsx

--- wp-content/themes/fictional-block-theme/our-blocks/banner.js
+++ wp-content/themes/fictional-block-theme/our-blocks/banner.js
@@ -7,10 +7,20 @@ wp.blocks.registerBlockTye(
     }// object
 );
 
-function EditComponent(){
-
+function EditComponent() {
+    return (
+        <div className="page-banner">
+            <div className="page-banner__bg-image" style={{backgroundImage: "url('/wp-content/fictional-block-theme/images/library-hero.jpg')"}}></div>
+            <div className="page-banner__content container t-center c-white">
+                <h1 className="headline headline--large">Welcome!</h1>
+                <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+                <h3 className="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+                <a href="#" className="btn btn--large btn--blue">Find Your Major</a>
+            </div>
+        </div >
+    )
 }
 
-function SaveComponent(){
+function SaveComponent() {
     return <p>Save</p>
 }
