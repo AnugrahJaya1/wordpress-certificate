ddce278d8fc9320a84d3e7ba6008049ca6b615e2
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-28 10:56:26 +0700

    implement tempalte for generic heading

---
+++ wp-content/themes/fictional-block-theme/build/generic-heading.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '42b0be3c82cc02a5e009');
--- wp-content/themes/fictional-block-theme/our-blocks/banner.js
+++ wp-content/themes/fictional-block-theme/our-blocks/banner.js
@@ -1,6 +1,7 @@
 import { InnerBlocks } from "@wordpress/block-editor"
+import { registerBlockType} from "@wordpress/blocks"
 
-wp.blocks.registerBlockType(
+registerBlockType(
     "ourblocktheme/banner",
     {
         title: "Banner",
@@ -23,7 +24,7 @@ function EditComponent() {
         <div className="page-banner">
             <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
             <div className="page-banner__content container t-center c-white">
-                <InnerBlocks allowedBlocks={["core/paragraph", "core/heading", "core/list"]}/>
+                <InnerBlocks allowedBlocks={["ourblocktheme/generic-heading"]}/>
             </div>
         </div >
     )
