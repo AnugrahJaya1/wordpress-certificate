6a7edef8f9f854d69d62f9722745ccd57b286110
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-01-28 10:35:42 +0700

    implement make theme block editable

--- wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '091993992da7b115e5e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => '2040ba6f1ddd59b1182f');
